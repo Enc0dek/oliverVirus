be60ccb80962c16b922d5223841140a4a63fbbb7
luis carlos annett lopez <papacalietesinaguacate@gmail.com>
2023-03-04 20:51:14 -0600

    add a loginfo function

--- oliver.py
+++ oliver.py
@@ -1,7 +1,12 @@
-import subprocess
+import subprocess,getpass
 
-try:
-    ipconfig = subprocess.check_output("ipconfig").decode("utf-8")
-finally:
+ipconfig = subprocess.check_output("ipconfig").decode("utf-8")
+sysinfo = subprocess.check_output("systeminfo").decode("utf-8")
+user = getpass.getuser()
+password = getpass.getpass()
+print(user, password)
+def loginfo(*val :object):
     with open("log.txt","w+") as f:
-        f.write(ipconfig)
+        for i in range(len(val)):
+            f.write(val[i-1])
+loginfo(ipconfig, sysinfo)
